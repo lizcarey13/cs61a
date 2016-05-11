4e6b7cc355632ef76975b491c72a96adb34c9781
Liz Carey <lizcarey13@gmail.com>
2016-05-10 22:56:27 -0700

    add sum_digits

--- practice/disc02.py
+++ practice/disc02.py
@@ -9,3 +9,31 @@ def countdown(n):
 		return
 	print(n)
 	countdown(n-1)
+
+"""Have countdown count up
+"""
+
+def countup(n):
+	if n <= 0:
+		return
+	countup(n-1)
+	print(n)
+
+"""Write a recursive function that sums the digits of a number n. 
+Assume n is positive. You might find the operators // and % useful.
+
+>>> sum_digits(7)
+7
+>>> sum_digits(30)
+3
+>>> sum_digits(228)
+12
+
+"""
+
+def sum_digits(n):
+	if n % 10 == n:
+		return n 
+	else: 
+		return (n % 10) + sum_digits(n // 10)
+		
